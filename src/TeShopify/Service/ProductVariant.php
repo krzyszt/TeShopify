3d53fef8ceab7b50277e95ba5e8b08df74aef83d
Krzysztof Kaminski <krzysztof.teonte@gmail.com>
2013-08-20 21:29:23 +0200

    Product Variant Service

---
+++ src/TeShopify/Service/ProductVariant.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace TeShopify\Service;
+
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\Factory as InputFactory;
+use Zend\InputFilter\InputFilterAwareInterface;
+use Zend\InputFilter\InputFilterInterface; 
+
+class ProductVariant implements InputFilterAwareInterface
+{
+    public function setInputFilter(InputFilterInterface $inputFilter)
+    {
+        throw new \Exception("Not used");
+    }
+    
+    public function getInputFilter()
+    {
+        if (!$this->inputFilter) {
+            $inputFilter = new InputFilter();
+ 
+            $factory = new InputFactory();
+ 
+            $inputFilter->add($factory->createInput(array(
+                'name'       => 'id',
+                'required'   => true,
+                'filters' => array(
+                    array('name'    => 'Int'),
+                ),
+            )));
+ 
+            $inputFilter->add($factory->createInput(array(
+                'name'     => 'title',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 100,
+                        ),
+                    ),
+                ),
+            )));
+ 
+            $inputFilter->add($factory->createInput(array(
+                'name'     => 'compare_at_price',
+                'required' => false,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 100,
+                        ),
+                    ),
+                ),
+            )));
+            
+            $inputFilter->add($factory->createInput(array(
+                'name'     => 'grams',
+                'required' => false,
+                'filters' => array(
+                    array('name'    => 'Int'),
+                )
+            )));
+            
+            $inputFilter->add($factory->createInput(array(
+                'name'     => 'inventory_management',
+                'required' => false,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 100,
+                        ),
+                    ),
+                ),
+            )));     
+            
+            $inputFilter->add($factory->createInput(array(
+                'name'     => 'inventory_policy',
+                'required' => false,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 100,
+                        ),
+                    ),
+                ),
+            )));        
+            
+            $inputFilter->add($factory->createInput(array(
+                'name'     => 'inventory_quantity',
+                'required' => false,
+                'filters' => array(
+                    array('name'    => 'Int'),
+                )
+            )));
+            
+            $inputFilter->add($factory->createInput(array(
+                'name'     => 'position',
+                'required' => false,
+                'filters' => array(
+                    array('name'    => 'Int'),
+                )
+            )));
+            
+            $inputFilter->add($factory->createInput(array(
+                'name'     => 'price',
+                'required' => false,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+            )));
+            
+            $inputFilter->add($factory->createInput(array(
+                'name'     => 'requires_shipping',
+                'required' => false,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+            )));
+            
+            $inputFilter->add($factory->createInput(array(
+                'name'     => 'sku',
+                'required' => false,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+            )));
+            
+            $inputFilter->add($factory->createInput(array(
+                'name'     => 'taxable',
+                'required' => false,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+            )));
+ 
+            $this->inputFilter = $inputFilter;        
+        }
+ 
+        return $this->inputFilter;
+    }
+    
+}
